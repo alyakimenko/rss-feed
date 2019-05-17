26708b3eb0a31b2d8eda6dbfc29704b9cce1d6fe
alyakimenko <yakimenkocntu@gmail.com>
2019-05-17 19:29:30 +0300

    base idea

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mmcdole/gofeed"
+)
 
 func main() {
-	fmt.Println("hello, rss-feed")
+	feedsList := os.Args[1:]
+
+	feedParser := gofeed.NewParser()
+	feed, err := feedParser.ParseURL(feedsList[0])
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	render(feed.Items)
+}
+
+func render(items []*gofeed.Item) {
+	for _, item := range items {
+		if len(item.Title) > 70 {
+			fmt.Printf("%v\n", item.Title)
+			fmt.Printf("%70v %s\n", "", item.Link)
+		} else {
+			fmt.Printf("%-70v %s\n", item.Title, item.Link)
+		}
+	}
 }
